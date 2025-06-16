981b9b1d0ffaef80028fcf0294e1c162cfacdcb9
Nouu188 <dg.pthinh@gmail.com>
2025-06-16 09:51:15 +0700

    Redis fixing

--- lib/redis.ts
+++ lib/redis.ts
@@ -4,6 +4,10 @@ declare global {
   var redis: Redis | undefined;
 }
 
+if (!process.env.REDIS_URL) {
+  throw new Error("REDIS_URL is not defined in environment variables.");
+}
+
 const redis = global.redis || new Redis(process.env.REDIS_URL!);
 
 if (process.env.NODE_ENV !== "production") global.redis = redis;
